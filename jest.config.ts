c8938cb32e2f1951ca07a1c1facfc82e64cccbbc
keiff3r <keiffer@blooo.io>
2024-08-22 13:31:30 +0200

    fix: repair tests

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,11 @@
-import baseConfig from "../../jest.config";
-
 export default {
-  ...baseConfig,
+  preset: "ts-jest",
+  testEnvironment: "node",
+  testRegex: ".test.ts$",
+  collectCoverage: true,
+  testPathIgnorePatterns: ["packages/*/lib-es", "packages/*/lib"],
+  coveragePathIgnorePatterns: ["packages/create-dapp"],
+  passWithNoTests: true,
+  // collectCoverageFrom: ["packages/**/src/*.ts"],
   rootDir: __dirname,
 };
